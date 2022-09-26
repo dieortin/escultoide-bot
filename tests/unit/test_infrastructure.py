5ff2f2e4e3134a5079527ace7eef8b1d34b89dbc
Diego Ortín <dieortin@gmail.com>
2022-09-26 03:37:59 +0200

    Commented out infrastructure test, as it is not implemented yet

--- tests/unit/test_infrastructure.py
+++ tests/unit/test_infrastructure.py
@@ -1,15 +1,15 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from escultoide_bot.infrastructure import EscultoideBot
+from backend.component import EscultoideBot
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in escultoide_bot/escultoide_bot_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = EscultoideBot(app, "escultoide-bot")
-    template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+# # example tests. To run these tests, uncomment this file along with the example
+# # resource in escultoide_bot/escultoide_bot_stack.py
+# def test_sqs_queue_created():
+#     app = core.App()
+#     stack = EscultoideBot(app, "escultoide-bot")
+#     template = assertions.Template.from_stack(stack)
+#
+# #     template.has_resource_properties("AWS::SQS::Queue", {
+# #         "VisibilityTimeout": 300
+# #     })
